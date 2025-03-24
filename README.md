22e21e494635b7d0378964f12a30f13bb624b64b
Ankit Gautam <antam122508@gmail.com>
2025-03-24 11:40:27 +0530

    Angular Intro UC1

--- README.md
+++
@@ -1,1 +0,0 @@
-# AngularJS_Intro
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Helloworld';
+
+  ngOnInit(): void{
+    this.title="Hello from BridgeLabz";
+  }
+}
